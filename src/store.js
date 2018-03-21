dda8157ac879928d84de8b2e1333b783ca3d6742
Senhung Wong <alexwongsenhung@gmail.com>
2018-03-21 00:18:02 -0700

    TDLS-1: Add id to Todo

--- src/store.js
+++ src/store.js
@@ -6,11 +6,11 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
     state: {
         todo: [
-            {task: 'front end basic structure', done: false},
-            {task: 'Readme', done: false},
-            {task: 'Adding Tasks Function', done: false},
-            {task: 'Set Tasks to done', done: false},
-            {task: 'HTTP request/response', done: false},
+            {id: 5, task: 'front end basic structure', done: false},
+            {id: 4, task: 'Readme', done: false},
+            {id: 3, task: 'Adding Tasks Function', done: false},
+            {id: 2, task: 'Set Tasks to done', done: false},
+            {id: 1, task: 'HTTP request/response', done: false},
         ]
     },
     getters: {
